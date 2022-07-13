35e8ac653049c36221b82ce4ae71109846a964e4
maroua-elk <109047660+maroua-elk@users.noreply.github.com>
2022-07-13 14:51:38 +0100

    queries simulator
    C'est le simulateur des requêtes SQL

---
+++ simulateur.java
@@ -0,0 +1,14 @@
+// importation des packages
+package project.project;
+
+//la classe simulateur contient les méthodes (requetes)
+
+public abstract class simulateur {
+    public abstract void create();
+
+    public abstract void select();
+
+    public abstract void update();
+
+    public abstract void delete();
+}
